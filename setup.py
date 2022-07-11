ba8cc696088ae876feb574f8969f698c40f153d4
Haigro <90160448+Haigro@users.noreply.github.com>
2022-07-11 13:33:27 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='DS5100_Module14',
+      version='0.1',
+      description='A demo we did for class!',
+      url='https://github.com/Haigro/DS5100_Module14.git',
+      author='Jose Diaz-Rivera & Tatev Kyosababyan',
+      author_email='jyd2hw@gmail.com, ers5af@virginia.edu',
+      license='MIT',
+      packages=['Module14_demo'])
