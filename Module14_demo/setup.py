ee09bbcfb50389fc061e695b3eaa36e89684b6e6
Haigro <90160448+Haigro@users.noreply.github.com>
2022-07-11 14:34:56 -0400

    Create setup.py

---
+++ Module14_demo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jabber',
+      version='0.1',
+      description='A function we did for class!',
+      #url='https://github.com/Haigro/DS5100_Module14.git',
+      author='Jose Diaz-Rivera & Tatev Kyosababyan',
+      author_email='jyd2hw@gmail.com, ers5af@virginia.edu',
+      license='MIT',
+      packages=['jabber'])
